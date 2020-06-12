1553967581439ef94ab02dbe1e8ab3bd943ed2ec
alfalavista <natalia.puchkova@gmail.com>
2020-06-11 20:36:40 -0700

    First init

---
+++ src/test/java/alfa/house/auth/AuthApplicationTests.java
@@ -0,0 +1,13 @@
+package alfa.house.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
